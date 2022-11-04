fbb4c6d09c3e74023a770a91baf7b1eaf8511862
ImKool-Gitch <71771827+ImKool-Gitch@users.noreply.github.com>
2022-11-04 19:38:14 +0100

    Create plane.c

---
+++ plane.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n = 7;
+    int t = 5;
+    char str[n];
+    char str2[n];
+    for (int i = 0; i < n; ++i) {
+        str[i] = '*';
+        str2[i] = '*';
+    }
+    printf("%s", str);
+    for (int i = 0; i < t; ++i) {
+        printf(" ");
+    }
+    printf("%s\n", str2);
+    str[0] = '*';
+    str[n-1] = '*';
+    str2[0] = '*';
+    str2[n-1] = '*';
+    for (int i = 1; i < n-1; ++i) {
+        str[i] = ' ';
+        str2[i] = ' ';
+    }
+    for (int i = 0; i < n; ++i) {
+        printf("%s", str);
+        for (int u = 0; u < t; ++u) {
+            printf(" ");
+        }
+        printf("%s\n", str2);
+    }
+    memset(str, '*', n);
+    memset(str2, '*', n);
+    printf("%s", str);
+    for (int i = 0; i < t; ++i) {
+        printf(" ");
+    }
+    printf("%s\n", str2);
+    return 0;
+}
